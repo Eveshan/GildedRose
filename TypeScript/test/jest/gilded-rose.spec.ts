4bbcd9dccab444d99261679286a4d44b771f5150
Nishka <nish1111nm@gmail.com>
2022-06-24 16:06:16 +0200

    Add tests

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -26,7 +26,7 @@ describe("Gilded Rose", () => {
   });
 
   describe("Sulfuras, Hand of Ragnaros", () => {
-    it("should not reduce quality or sellin for legendary items", () => {
+    it("should not change quality or sellin for legendary items", () => {
       const gildedRose = new GildedRose([
         new Item("Sulfuras, Hand of Ragnaros", 10, 20),
       ]);
@@ -35,6 +35,72 @@ describe("Gilded Rose", () => {
         name: "Sulfuras, Hand of Ragnaros",
         sellIn: 10,
         quality: 20,
+      });
+    });
+  });
+
+  describe("Aged Brie", () => {
+    it("should increase by 1 in quality", () => {
+      const gildedRose = new GildedRose([
+        new Item("Aged Brie", 10, 20),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0]).toEqual({
+        name: "Aged Brie",
+        sellIn: 9,
+        quality: 21,
+      });
+    });
+
+    describe("within sell in", () => {
+      it("should increase by 1 in quality when given quality is less than 50", () => {
+        const gildedRose = new GildedRose([
+          new Item("Aged Brie", 3, 49),
+        ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0]).toEqual({
+          name: "Aged Brie",
+          sellIn: 2,
+          quality: 50,
+        });
+      });
+
+      it("should not change in quality when given quality is 50", () => {
+        const gildedRose = new GildedRose([
+          new Item("Aged Brie", 3, 50),
+        ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0]).toEqual({
+          name: "Aged Brie",
+          sellIn: 2,
+          quality: 50,
+        });
+      });
+    });
+
+    describe("outside sell in", () => {
+      it("should increase by 2 in quality when given quality is less than 49", () => {
+        const gildedRose = new GildedRose([
+          new Item("Aged Brie", -1, 48),
+        ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0]).toEqual({
+          name: "Aged Brie",
+          sellIn: -2,
+          quality: 50,
+        });
+      });
+
+      it("should not allow quality greater than 50", () => {
+        const gildedRose = new GildedRose([
+          new Item("Aged Brie", -1, 49),
+        ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0]).toEqual({
+          name: "Aged Brie",
+          sellIn: -2,
+          quality: 50,
+        });
       });
     });
   });
