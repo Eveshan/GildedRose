21783f94e13ce2af2f6e9afc6e242090941ae6ad
Nishka <nish1111nm@gmail.com>
2022-06-24 15:24:08 +0200

    add describes to test

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,35 +1,41 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
 describe("Gilded Rose", () => {
-  it("should reduce sellin value on random item", () => {
-    const gildedRose = new GildedRose([new Item("BattleAxe", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0]).toEqual({
-      name: "BattleAxe",
-      sellIn: -1,
-      quality: 0,
+
+
+  describe("Given Random Item", () => {
+    it("should reduce sellin value", () => {
+      const gildedRose = new GildedRose([new Item("BattleAxe", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0]).toEqual({
+        name: "BattleAxe",
+        sellIn: -1,
+        quality: 0,
+      });
+    });
+
+    it("should reduce sellin by 2 if its value is negative", () => {
+      const gildedRose = new GildedRose([new Item("BattleAxe", -1, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0]).toEqual({
+        name: "BattleAxe",
+        sellIn: -2,
+        quality: 0,
+      });
     });
   });
 
-  it("should reduce sellin by 2 if its value is negative on random item", () => {
-    const gildedRose = new GildedRose([new Item("BattleAxe", -1, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0]).toEqual({
-      name: "BattleAxe",
-      sellIn: -2,
-      quality: 0,
-    });
-  });
-
-  it("should not reduce quality or sellin for legendary items", () => {
-    const gildedRose = new GildedRose([
-      new Item("Sulfuras, Hand of Ragnaros", 10, 20),
-    ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0]).toEqual({
-      name: "Sulfuras, Hand of Ragnaros",
-      sellIn: 10,
-      quality: 20,
+  describe("Sulfuras, Hand of Ragnaros", () => {
+    it("should not reduce quality or sellin for legendary items", () => {
+      const gildedRose = new GildedRose([
+        new Item("Sulfuras, Hand of Ragnaros", 10, 20),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0]).toEqual({
+        name: "Sulfuras, Hand of Ragnaros",
+        sellIn: 10,
+        quality: 20,
+      });
     });
   });
 });
