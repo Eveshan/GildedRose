eabf277d346a5302dfea64fcddc436a81409d12a
Eveshan Moodley <Eveshan.Moodley@chillisoft.co.za>
2022-06-24 15:20:29 +0200

    Updated tests

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,35 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
 describe("Gilded Rose", () => {
-  it("should foo", () => {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+  it("should reduce sellin value on random item", () => {
+    const gildedRose = new GildedRose([new Item("BattleAxe", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0]).toEqual({
+      name: "BattleAxe",
+      sellIn: -1,
+      quality: 0,
+    });
+  });
+
+  it("should reduce sellin by 2 if its value is negative on random item", () => {
+    const gildedRose = new GildedRose([new Item("BattleAxe", -1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0]).toEqual({
+      name: "BattleAxe",
+      sellIn: -2,
+      quality: 0,
+    });
+  });
+
+  it("should not reduce quality or sellin for legendary items", () => {
+    const gildedRose = new GildedRose([
+      new Item("Sulfuras, Hand of Ragnaros", 10, 20),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0]).toEqual({
+      name: "Sulfuras, Hand of Ragnaros",
+      sellIn: 10,
+      quality: 20,
+    });
   });
 });
