5debd200bcd1381ff341c3e213d83753a1b039aa
Nishka Maharaj <Nishka.Maharaj@chillisoft.co.za>
2022-07-01 15:27:54 +0200

    format

--- TypeScript/app/ItemUpdater.ts
+++ TypeScript/app/ItemUpdater.ts
@@ -45,9 +45,9 @@ export function update(item: Item) {
         item.quality = item.quality - item.quality;
       }
     }
-    else       if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
+    else if (item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
   }
   return item;
 }
